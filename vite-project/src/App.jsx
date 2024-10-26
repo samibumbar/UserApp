1bb868324a8294b87fbf314ed1581c13bf5a1397
Samuel Bumbar <134549221+samibumbar@users.noreply.github.com>
2024-10-26 23:22:49 +0200

    remove

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -31,7 +31,7 @@ function App() {
 
   return (
     <div className="container">
-      <Router basename="/UserApp">
+      <Router>
         {user && <Navbar />}
         <Routes>
           <Route path="/" element={<AuthForm />} />
