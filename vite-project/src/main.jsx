d7e3ff537048d4c9373729eda697f973fb6afe62
Samuel Bumbar <134549221+samibumbar@users.noreply.github.com>
2024-12-09 08:20:39 -0800

    Update main.jsx

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider, CssBaseline } from "@mui/material"; // Import ThemeProvider și CssBaseline
+import { ThemeProvider, CssBaseline } from "@mui/material"; 
 import App from "./App";
-import theme from "./theme"; // Import tema creată
+import theme from "./theme"; 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <CssBaseline /> {/* Aplică fundalul și resetează stilurile */}
+      <CssBaseline /> 
       <App />
     </ThemeProvider>
   </React.StrictMode>
